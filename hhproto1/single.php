f043c359d17b76c4eac26c3875589878d3fc8571
Max Norton <maxbnorton@gmail.com>
2015-06-02 13:26:00 -0700

    only generate post navigation if next or previous posts exist

--- hhproto1/single.php
+++ hhproto1/single.php
@@ -17,42 +17,50 @@
 							$next_post = get_next_post();
 						?>
 						
-						<div class="post-navigation">
+						<?php if ( get_next_posts_link() || get_previous_posts_link() ) { ?>
 							
-							<div class="post-navigation-inner">
-						
-								<?php
-								if (!empty( $prev_post )): ?>
+							<div class="post-navigation">
 								
-									<div class="post-nav-prev">
-										<p><?php _e('Previous', 'lovecraft'); ?></p>
-										<h4>
-											<a href="<?php echo get_permalink( $prev_post->ID ); ?>" title="<?php _e('Previous post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($prev_post) ); ?>">
-												<?php echo get_the_title($prev_post); ?>
-											</a>
-										</h4>
-									</div>
-								<?php endif; ?>
-								
-								<?php
-								if (!empty( $next_post )): ?>
+								<div class="post-navigation-inner">
+							
+									<?php
+									if (!empty( $prev_post )): ?>
 									
-									<div class="post-nav-next">					
-										<p><?php _e('Next', 'lovecraft'); ?></p>
-										<h4>
-											<a title="<?php _e('Next post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($next_post) ); ?>" href="<?php echo get_permalink( $next_post->ID ); ?>">
-												<?php echo get_the_title($next_post); ?>
-											</a>
-										</h4>
-									</div>
-							
-								<?php endif; ?>
+										<div class="post-nav-prev">
+											<p><?php _e('Previous', 'lovecraft'); ?></p>
+											<h4>
+												<a href="<?php echo get_permalink( $prev_post->ID ); ?>" title="<?php _e('Previous post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($prev_post) ); ?>">
+													<?php echo get_the_title($prev_post); ?>
+												</a>
+											</h4>
+										</div>
+									<?php endif; ?>
+									
+									<?php
+									if (!empty( $next_post )): ?>
+										
+										<div class="post-nav-next">					
+											<p><?php _e('Next', 'lovecraft'); ?></p>
+											<h4>
+												<a title="<?php _e('Next post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($next_post) ); ?>" href="<?php echo get_permalink( $next_post->ID ); ?>">
+													<?php echo get_the_title($next_post); ?>
+												</a>
+											</h4>
+										</div>
 								
-								<div class="clear"></div>
+									<?php endif; ?>
+									
+									<div class="clear"></div>
+								
+								</div> <!-- /post-navigation-inner -->
 							
-							</div> <!-- /post-navigation-inner -->
+							</div> <!-- /post-navigation -->
 						
-						</div> <!-- /post-navigation -->
+						<?php } else { ?>
+
+						<div class="no-post-nav-padding"></div>
+
+						<?php } ?>
 				
 						<div class="post-header">
 							
