8111bcf50f91bd928e2918f0654b9f4dc5c41bee
Max Norton <maxbnorton@gmail.com>
2015-06-02 13:08:25 -0700

    move post navigation to top of page

--- hhproto1/single.php
+++ hhproto1/single.php
@@ -11,6 +11,48 @@
 				<div id="post-<?php the_ID(); ?>" <?php post_class('single'); ?>>
 							
 					<div class="post-inner">
+
+						<?php
+							$prev_post = get_previous_post();
+							$next_post = get_next_post();
+						?>
+						
+						<div class="post-navigation">
+							
+							<div class="post-navigation-inner">
+						
+								<?php
+								if (!empty( $prev_post )): ?>
+								
+									<div class="post-nav-prev">
+										<p><?php _e('Previous', 'lovecraft'); ?></p>
+										<h4>
+											<a href="<?php echo get_permalink( $prev_post->ID ); ?>" title="<?php _e('Previous post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($prev_post) ); ?>">
+												<?php echo get_the_title($prev_post); ?>
+											</a>
+										</h4>
+									</div>
+								<?php endif; ?>
+								
+								<?php
+								if (!empty( $next_post )): ?>
+									
+									<div class="post-nav-next">					
+										<p><?php _e('Next', 'lovecraft'); ?></p>
+										<h4>
+											<a title="<?php _e('Next post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($next_post) ); ?>" href="<?php echo get_permalink( $next_post->ID ); ?>">
+												<?php echo get_the_title($next_post); ?>
+											</a>
+										</h4>
+									</div>
+							
+								<?php endif; ?>
+								
+								<div class="clear"></div>
+							
+							</div> <!-- /post-navigation-inner -->
+						
+						</div> <!-- /post-navigation -->
 				
 						<div class="post-header">
 							
@@ -74,48 +116,6 @@
 						<?php endif; ?>
 					
 					</div> <!-- /post-inner -->
-					
-					<?php
-						$prev_post = get_previous_post();
-						$next_post = get_next_post();
-					?>
-					
-					<div class="post-navigation">
-						
-						<div class="post-navigation-inner">
-					
-							<?php
-							if (!empty( $prev_post )): ?>
-							
-								<div class="post-nav-prev">
-									<p><?php _e('Previous', 'lovecraft'); ?></p>
-									<h4>
-										<a href="<?php echo get_permalink( $prev_post->ID ); ?>" title="<?php _e('Previous post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($prev_post) ); ?>">
-											<?php echo get_the_title($prev_post); ?>
-										</a>
-									</h4>
-								</div>
-							<?php endif; ?>
-							
-							<?php
-							if (!empty( $next_post )): ?>
-								
-								<div class="post-nav-next">					
-									<p><?php _e('Next', 'lovecraft'); ?></p>
-									<h4>
-										<a title="<?php _e('Next post', 'lovecraft'); echo ': ' . esc_attr( get_the_title($next_post) ); ?>" href="<?php echo get_permalink( $next_post->ID ); ?>">
-											<?php echo get_the_title($next_post); ?>
-										</a>
-									</h4>
-								</div>
-						
-							<?php endif; ?>
-							
-							<div class="clear"></div>
-						
-						</div> <!-- /post-navigation-inner -->
-					
-					</div> <!-- /post-navigation -->
 														
 				</div> <!-- /post -->
 											                        
