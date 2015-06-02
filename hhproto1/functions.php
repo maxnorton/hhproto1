981c3bc4d0f626cff8e4f1e6ec1fd39461bfee98
Max Norton <maxbnorton@gmail.com>
2015-06-02 12:06:36 -0700

    add google platform to function theme_enqueue_dependencies

--- hhproto1/functions.php
+++ hhproto1/functions.php
@@ -1,11 +1,12 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_dependencies' );
+function theme_enqueue_dependencies() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array('parent-style')
     );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+	wp_enqueue_script( 'google-platform', 'https://apis.google.com/js/platform.js' );
 }
 ?>
