bb814af11a4edab8fbed4b11bcfea369009bf98e
Max Norton <maxbnorton@gmail.com>
2015-07-12 16:31:10 -0700

    transfer sidebar bio from hardcode to widget

--- hhproto1/sidebar.php
+++ hhproto1/sidebar.php
@@ -1,19 +1,4 @@
 <div class="sidebar">
-
-	<div class="widget widget-bio">
-		<div class="bio-image">
-			<img src="<?php echo get_stylesheet_directory_uri() . '/images/heidi.jpg'; ?>" alt="Heidi Hall" />
-		</div>
-		<div class="widget-title">
-			<h2>Heidi Hall</h2>
-		</div>
-		<div class="widget-content">
-			<p>Heidi Hall is a lifelong resident of Northern California, and is excited and ready to serve as the next Congresswoman for the 1st Congressional District. Raised by teachers, and as a daughter and mother of veterans, she knows full well the value of hard work. She is prepared to provide the vision and leadership that the people of the North State need.</p>
-		
-			<p><a href="mailto:email@heidihallemail.com"><i class="fa fa-envelope-o" style="padding-right:15px;"></i>email@heidihallemail.com</a></p>
-		</div>
-	</div>	
-
 	<?php 
 		if (is_active_sidebar('sidebar')) { 
 			
