8575c6836e7469397798d4d2ea233a9f2607b16a
Max Norton <maxbnorton@gmail.com>
2015-08-11 17:08:19 -0700

    add class to archive nav links

--- hhproto1/functions.php
+++ hhproto1/functions.php
@@ -9,4 +9,52 @@ function theme_enqueue_dependencies() {
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
 	wp_enqueue_script( 'google-platform', 'https://apis.google.com/js/platform.js' );
 }
+
+// Archive navigation function
+function hhproto1_archive_navigation() {
+	
+	global $wp_query;
+	global $paged;
+	
+	if ( $wp_query->max_num_pages > 1 ) : ?>
+				
+		<div class="archive-navigation">
+			
+			<div class="fleft">
+				
+				<p><?php printf( __('Page %s of %s', 'lovecraft'), $paged, $wp_query->max_num_pages ); ?></p>
+				
+			</div>
+			
+			<div class="fright">
+				
+				<?php if ( get_previous_posts_link() ) : ?>
+				
+					<p class="previous-posts">
+					
+						<?php echo get_previous_posts_link( '&larr; ' . __('Previous', 'lovecraft')); ?>
+					
+					</p>
+				
+				<?php endif; ?>
+				
+				<?php if ( get_next_posts_link() ) : ?>
+				
+					<p class="next-posts">
+					
+						<?php echo get_next_posts_link( __('Next', 'lovecraft') . ' &rarr;'); ?>
+						
+					</p>
+				
+				<?php endif; ?>
+			
+			</div>
+			
+			<div class="clear"></div>
+						
+		</div> <!-- /archive-nav -->
+						
+	<?php endif;
+}
+
 ?>
