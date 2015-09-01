5ebc580b9a2f4f5268194fc211b3e30b7e291d21
Max Norton <maxbnorton@gmail.com>
2015-09-01 15:49:55 -0700

    update single.php so it calls the appropriate content template on basis of format

--- heidi/single.php
+++ heidi/single.php
@@ -56,65 +56,9 @@
 							
 							</div> <!-- /post-navigation -->
 						
-						<?php } else { ?>
-
-						<div class="no-post-nav-padding"></div>
-
 						<?php } ?>
 				
-						<div class="post-header">
-							
-						    <h2 class="post-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-						    
-						    <div class="post-meta">
-			    
-							    <p class="post-author"><span><?php _e('By','lovecraft'); ?> </span><?php the_author_posts_link(); ?></p>
-								<p class="post-date"><span><?php _e('On','lovecraft'); ?> </span><a href="<?php the_permalink(); ?>"><?php the_time(get_option('date_format')); ?></a></p>
-								<?php edit_post_link('Edit', '<p>', '</p>'); ?>
-							    
-						    </div>
-						    	    
-						</div> <!-- /post-header -->
-						
-						<?php if ( get_the_content() ) : ?>
-		
-							<div class="post-content">
-							
-								<?php the_content(); ?>
-								
-								<?php 
-							    	$args = array(
-										'before'           => '<div class="clear"></div><p class="page-links"><span class="title">' . __( 'Pages:','lovecraft' ) . '</span>',
-										'after'            => '</p>',
-										'link_before'      => '<span>',
-										'link_after'       => '</span>',
-										'separator'        => '',
-										'pagelink'         => '%',
-										'echo'             => 1
-									);
-						    	
-						    		wp_link_pages($args); 
-								?>
-							
-								<div class="share">
-									<span>
-										<div class="fb-share-button" data-href="<?php the_permalink(); ?>" data-layout="button_count"></div>
-									</span>
-									<span>
-										<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="Heidi Hall: <?php the_title(); ?>">Tweet</a>
-										<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-									</span>
-									<span>
-										<g:plusone href="<?php the_permalink(); ?>"></g:plusone>
-									</span>
-									<span>
-										<a href="#" onclick="alert('email dialog box not yet built'); e.preventdefault();"><i class="fa fa-envelope-o" style="padding-right:15px;"></i> Email this</a>
-									</span>
-								</div>
-
-							</div>
-						
-						<?php endif; ?>
+						<?php get_template_part( 'content', get_post_format() ); ?>
 						
 						<?php if ( has_tag() ) : ?>
 						
